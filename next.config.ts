b52c99e89e6210e39fcbc581cc8f9382e39d8c10
ritanksaxena2024 <ritank.s@netconnectglobal.com>
2025-07-15 14:50:58 +0530

    final route

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,21 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development', 
+  disable: process.env.NODE_ENV === 'development',
 });
 
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = withPWA(nextConfig);
