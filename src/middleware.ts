7e066cc5f4f7cae8febe4bcccb29465748f1adf4
ritanksaxena2024 <ritank.s@netconnectglobal.com>
2025-07-15 14:24:08 +0530

    Build error

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,7 +17,7 @@ export async function middleware(request: NextRequest) {
       }
 
       return NextResponse.next();
-    } catch (err) {
+    } catch  {
       const response = NextResponse.redirect(new URL('/login', request.url));
       response.cookies.set('token', '', { maxAge: 0 });
       return response;
